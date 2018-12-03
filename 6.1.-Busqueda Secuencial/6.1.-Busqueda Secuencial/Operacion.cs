41012e129b8fe938b7a54285be72b7cedd164d07
CMRamirezC <43421100+CMRamirezC@users.noreply.github.com>
2018-12-02 19:40:34 -0800

    Add files via upload

---
+++ 6.1.-Busqueda Secuencial/6.1.-Busqueda Secuencial/Operacion.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._Busqueda_Secuencial
+{
+    class Operacion
+    {
+        public bool BusquedaInt(List<int> lista, int item)// metodo para realizar busqueda de tipo de dato entero
+        {
+            int posicion = 0;
+            bool encontrado = false;
+            while (posicion < lista.Count)//ciclo para recorrer la lista
+            {
+                if (lista[posicion] == item)// si el elemento de la lista en la posicion actual es igual al elemento item
+                {
+                    encontrado = true;
+                    return encontrado;// retorna true
+                }
+                else// si no existe un elemento igual
+                {
+                    posicion = posicion + 1;// aumenta la posicion en la lista
+                }
+            }
+            return encontrado;// retorna la variable de tipo bool 
+        }
+        public bool BusquedaString(List<string> lista, string item)// el mismo metodo pero para tipo de datos de texto
+        {
+            int posicion = 0;
+            bool encontrado = false;
+            while (posicion < lista.Count)
+            {
+                if (lista[posicion] == item)
+                {
+                    encontrado = true;
+                    return encontrado;
+                }
+                else
+                {
+                    posicion = posicion + 1;
+                }
+            }
+            return encontrado;
+        }
+    }
+}
---
+++ 6.1.-Busqueda Secuencial/6.1.-Busqueda Secuencial/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6._1._Busqueda_Secuencial
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = " 6.1 Busqueda Secuencial ";
+            Operacion op = new Operacion();
+            Console.WriteLine(" Ejemplo 1: ");
+            List<int> Calificaciones = new List<int> { 90, 100, 80, 78, 83 };// lista de calificacion de tipo entero
+            Console.Write(" Que calificacion desea buscar? (70 - 100): ");// se le pregunta al usuario que calificacion desea buscar en la lista
+            int item = int.Parse(Console.ReadLine());// se guarda el dato ingresado en una variable 
+            bool respuesta = op.BusquedaInt(Calificaciones, item);// se guarda en una variable de tipo bool el retorno del metodo de busqueda, mandando como parametros la lista y el dato ingresado
+            if (respuesta == true) // si la respuesta es true 
+                Console.WriteLine(" Calificacion encontrada! ");// imprime mensaje de que encontro la calificacion 
+            else // si es false
+                Console.WriteLine(" Calificacion no encontrada! ");// imprime mensaje de que no encontro la calificacion
+            Console.WriteLine();
+
+            Console.WriteLine(" Ejemplo 2: ");
+            List<string> Nombres = new List<string> { "Cesar", "Samantha", "Pedro", "Jose", "Dulce" };// lista de nombres de tipo string 
+            Console.Write(" Que nombre desea buscar en la lista?: ");// se le pregunta al usuario que nombre desea buscar en la lista 
+            string name = Console.ReadLine();// se guarda el dato ingresado en una variable string 
+            bool respuesta2 = op.BusquedaString(Nombres, name);// se guarda en una variable de tipo bool el retorno del metodo de busqueda, mandando como parametros la lista y el dato ingresado
+            if (respuesta2 == true)// si la respuesta es true 
+                Console.WriteLine(" Nombre encontrado! ");// imprime mensaje de que encontro el nombre
+            else // si es false
+                Console.WriteLine(" Nombre no encontrado! ");// imprime mensaje de que no encontro el nombre
+            Console.WriteLine();
+
+        }
+    }
+}
